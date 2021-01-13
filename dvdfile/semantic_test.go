59030cbe8d6c5751c1843dc41903ae9dc1349d78
zgl <376281732@qq.com>
2021-01-13 16:40:16 +0800

    Add test function.

--- dvdfile/semantic_test.go
+++ dvdfile/semantic_test.go
@@ -2,31 +2,7 @@ package dvdfile
 
 import "testing"
 
-const checkMark = "\u2713"
-const ballotX = "\u2717"
-
 func TestTransToLines(t *testing.T) {
-    text := `    para1, line1
-    para1, line2
-
-`
-    wants := [][]byte{
-        []byte("    para1, line1\n"),
-        []byte("    para1, line2\n"),
-        []byte("\n"),
-    }
-    gotLines := TransToLines([]byte(text))
-    t.Log("Given the need to transform text into lines no mem.")
-    {
-        for i, gotLine := range gotLines {
-            if !sliceEq(wants[i], gotLine) {
-                t.Logf("want:%s get:%s", wants[i], gotLine)
-            }
-        }
-    }
-}
-
-func TestTransToLinesMem(t *testing.T) {
     text := []byte(`    para1, line1
     para1, line2
 
@@ -36,32 +12,18 @@ func TestTransToLinesMem(t *testing.T) {
         []byte("    para1, line2\n"),
         []byte("\n"),
     }
-    gotLines := TransToLinesMem(text)
-    t.Log("Given the need to transform text into lines with mem.")
+    gotLines := TransToLines(text)
+    t.Log("Given the need to transform text into lines.")
     {
-        t.Log("\t\tTest the content is correctly parsed")
-        {
-            for i, gotLine := range gotLines {
-                if !sliceEq(wants[i], gotLine) {
-                    t.Logf("want:%s get:%s", wants[i], gotLine)
-                }
-            }
-        }
-        t.Log("\t\tTest the memory is correctly allocate")
-        {
-            gotLinesModify := append(gotLines, []byte("New line"))
-            buf := make([]byte, 0)
-            for _, line := range gotLinesModify {
-                buf = append(buf, line...)
-            }
-            if sliceEq(buf, text) {
-                t.Log("memory allocate incorrectly")
+        for i, gotLine := range gotLines {
+            if !sliceEq(wants[i], gotLine) {
+                t.Errorf("want:%s get:%s", wants[i], gotLine)
             }
         }
     }
 }
 
-func TestTransToParasMem(t *testing.T) {
+func TestTransToParas(t *testing.T) {
     text := []byte(`    para1, line1
 
 
@@ -79,14 +41,36 @@ func TestTransToParasMem(t *testing.T) {
             []byte("\n"),
         },
     }
-    gots := TransToParasMem(text)
+    gots := TransToParas(text)
     t.Log("Given the need for transform text into parameters")
     {
         for i, gotPara := range gots {
             for j, gotLine := range gotPara {
                 if !sliceEq(wants[i][j], gotLine) {
-                    t.Logf("want %s get %s", wants[i][j], gotLine)
+                    t.Errorf("want %s get %s", wants[i][j], gotLine)
                 }
+            }
+        }
+    }
+}
+
+func TestTransToParasNoLine(t *testing.T) {
+    text := []byte(`    para1, line1
+
+
+    para2, line1
+
+`)
+    wants := [][]byte{
+        []byte("    para1, line1\n\n\n"),
+        []byte("    para2, line1\n\n"),
+    }
+    gots := TransToParasNoLine(text)
+    t.Log("Given the need for transform text into parameters without line concept")
+    {
+        for i, gotPara := range gots {
+            if !sliceEq(wants[i], gotPara) {
+                t.Errorf("want %s get %s", wants[i], gotPara)
             }
         }
     }
